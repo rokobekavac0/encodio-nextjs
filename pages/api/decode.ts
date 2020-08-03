032d026ea50f66476860a5595d6d8c91da07e1c3
Roko <roko.bekavac2003@gmail.com>
2020-08-03 23:48:34 +0200

    add encodedWords to decode API

--- pages/api/decode.ts
+++ pages/api/decode.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { decodeString, IDecodeStringRet } from "../../utils/decoder";
 import { NullablePartial } from "../../utils/partialNull";
+import { CLIENT_RENEG_LIMIT } from "tls";
 
 interface IDecodeType extends NullablePartial<IDecodeStringRet> {
   error: string | null;
@@ -8,20 +9,17 @@ interface IDecodeType extends NullablePartial<IDecodeStringRet> {
 
 function decodeHandler(req: NextApiRequest, res: NextApiResponse) {
   const { method, body } = req;
-
+  const parsedBody = JSON.parse(body);
   switch (method) {
     case "POST":
       try {
-        const { isEncryped, amount }: IDecodeStringRet = decodeString(
-          body.data,
-          process.env.NUM_CODE?.split(" ").map((e) => parseInt(e))!
-        );
+        if (!parsedBody.data) throw "no data in body";
+        const { isEncryped, amount, encodedWords }: IDecodeStringRet = decodeString(parsedBody.data, process.env.NUM_CODE?.split(" ").map((e) => parseInt(e))!);
 
-        res
-          .status(200)
-          .json({ error: null, amount, isEncryped } as IDecodeType);
+        res.status(200).json({ error: null, amount, isEncryped, encodedWords } as IDecodeType);
       } catch (error) {
-        res.status(500).json({ error: error } as IDecodeType);
+        console.log(error);
+        res.status(200).json({ error: error } as IDecodeType);
       }
       break;
     default:
