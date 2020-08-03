42884c1addce268fb5bce20ae74833469bf607df
Roko <roko.bekavac2003@gmail.com>
2020-08-03 23:47:50 +0200

    add link

--- components/SwitchPage.tsx
+++ components/SwitchPage.tsx
@@ -1,14 +1,20 @@
 import { css } from "@emotion/css";
 import tw from "@tailwindcssinjs/macro";
-
+import Link from "next/link";
 interface SwitchPageProps {
   page: "encode" | "decode";
 }
 
 export const SwithPage = ({ page }: SwitchPageProps) => {
   return (
-    <a>
-      Need to <strong className={css(tw`font-bold`)}>{page}</strong> some text?
-    </a>
+    <div>
+      Need to{" "}
+      <strong className={css(tw`font-bold`)}>
+        <Link href={page === "decode" ? "/decode" : "/"}>
+          <a>{page}</a>
+        </Link>
+      </strong>{" "}
+      some text?
+    </div>
   );
 };
