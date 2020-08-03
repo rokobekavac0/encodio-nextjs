454c06ce4f1a866c49be39e3be43a112d6845bac
Roko <roko.bekavac2003@gmail.com>
2020-08-03 23:46:33 +0200

    rename

--- utils/errorButify.ts
+++
@@ -1,8 +0,0 @@
-export function beautifyError(error: string) {
-  switch (error) {
-    case "no letters matching":
-      return "Input text too short";
-    default:
-      return "Unknow error, try again later...";
-  }
-}
