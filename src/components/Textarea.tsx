19f0894c0337a39f58bdd73e47ceff9ec593da83
Roko <roko.bekavac2003@gmail.com>
2020-08-05 16:04:26 +0200

    fix font size

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -24,7 +24,7 @@ export const Textarea: React.FC<TextareaProps> = ({ id, showSpinner, hasSpinner,
         {labelText}
       </label>
       <div className={css(tw`relative`)}>
-        <Spinner show={showSpinner} className={css(tw`absolute top-4 text-green-500 right-0`, spinnerClassName)} />
+        <Spinner show={showSpinner} className={css(tw`absolute top-4 text-secundaryFooter right-0`, spinnerClassName)} />
         <textarea
           id={id}
           value={value}
@@ -34,7 +34,7 @@ export const Textarea: React.FC<TextareaProps> = ({ id, showSpinner, hasSpinner,
           onChange={onChange}
           placeholder={placeHolder}
           className={css(
-            tw`shadow resize-none appearance-none tracking-normal mt-2 py-2 h-32 md:h-40 border bg-black w-full text-white font-mono mb-3 rounded-none`,
+            tw`shadow resize-none  sm:text-base text-sm appearance-none font-bold tracking-normal mt-2 py-2 h-32 md:h-40 border bg-black w-full text-white font-mono mb-3 rounded-none`,
             hasError ? tw`text-red-700` : tw``,
             hasSpinner ? tw`pl-3 pr-10` : tw` px-3`
           )}
