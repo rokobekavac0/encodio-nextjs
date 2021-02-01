baf20e118ec55a0aedd6ff62c5565fec72842473
rokobekavac0 <roko.beakavac2003@gmail.com>
2021-02-01 21:43:50 +0100

    update imports, use new hooks

--- src/pages/decode.tsx
+++ src/pages/decode.tsx
@@ -1,120 +1,70 @@
 import { css } from "@emotion/css";
 import tw from "@tailwindcssinjs/macro";
-import { Header } from "../components/Header";
-import { SwithPage } from "../components/SwitchPage";
-import { Faq } from "../components/Faq";
-import { useState, useEffect, useRef } from "react";
-import { useDebounce } from "use-debounce";
-import { Footer } from "../components/Footer";
-import { Spinner } from "../components/Spinner";
+import { Header } from "@/components/Header";
+import { SwithPage } from "@/components/SwitchPage";
+import { Faq } from "@/components/Faq";
+import { useState, useRef } from "react";
+import { Footer } from "@/components/Footer";
+import { Spinner } from "@/components/Spinner";
 import Highlighter from "react-highlight-words";
-import { Textarea } from "../components/Textarea";
-import { Stats } from "../components/Stats";
-
-export interface IDecodedData {
-  amount?: number | null;
-  isEncryped?: number | null;
-  encodedWords?: Array<string> | null;
-  error?: string | null;
-}
+import { Textarea } from "@/components/Textarea";
+import { Stats } from "@/components/Stats";
+import { Layout } from "@/components/Layout";
+import { useDecode } from "@/hooks/useDecode";
 
 const DecodePage = () => {
-  const [decodedData, setdecodedData] = useState<IDecodedData | null>();
+  const inputRef = useRef<HTMLTextAreaElement>(null);
   const [input, setInput] = useState<string>("");
-  const [isSearching, setIsSearching] = useState(false);
-  const [error, setError] = useState<boolean | null>(null);
-  const inputref = useRef<HTMLTextAreaElement>(null);
-  const [debouncedInputValue] = useDebounce(inputref.current?.value, 500);
-  const [debouncedLoading] = useDebounce(inputref.current?.value, 500, { leading: true });
-
-  useEffect(() => {
-    setIsSearching(true);
-    if (inputref.current?.value === "") {
-      setIsSearching(false);
-    }
-  }, [debouncedLoading]);
-
-  useEffect(() => {
-    setIsSearching(true);
-    if (debouncedInputValue === "") {
-      setdecodedData(null);
-      setIsSearching(false);
-    }
-    debouncedInputValue &&
-      (async () => {
-        try {
-          const req = await fetch(`${window.location.origin}/api/decode`, {
-            body: JSON.stringify({ data: debouncedInputValue }),
-            method: "POST",
-          });
-          const dat = await req.json();
-          if (dat.error) throw new Error(dat.error);
-          setError(false);
-          console.log(dat);
-          setdecodedData(dat);
-        } catch (error) {
-          setdecodedData(null);
-          setError(true);
-        } finally {
-          setIsSearching(false);
-        }
-      })();
-  }, [debouncedInputValue]);
+  const { decodedData, error, isSearching } = useDecode({ inputRef: inputRef });
 
   return (
-    <div className={css(tw`bg-black`)}>
-      <div className={css(tw` max-w-screen-xl text-white pt-8 md:pt-10 md:px-6 px-8 flex flex-col ml-auto mr-auto`)}>
-        <div className={css(tw``)}>
+    <Layout>
+      <div className={css(tw`bg-black`)}>
+        <div className={css(tw` max-w-screen-xl text-white pt-8 md:pt-10 md:px-6 px-8 flex flex-col ml-auto mr-auto`)}>
           <div className={css(tw``)}>
-            <Header />
+            <div className={css(tw``)}>
+              <Header />
+            </div>
           </div>
-        </div>
 
-        <div className={css(tw`text-white font-light pt-4 text-right`)}>
-          <SwithPage page={"encode"} />
-        </div>
+          <div className={css(tw`text-white font-light pt-4 text-right`)}>
+            <SwithPage page={"encode"} />
+          </div>
 
-        <div>
-          <div className={css(tw`w-full pt-6 `)}>
-            <div className={css(tw`md:grid grid-cols-2 col-gap-9`)}>
-              <div>
-                <Textarea
-                  id={"inputD"}
-                  labelText={"Input text"}
-                  placeHolder={"Type or paste text"}
-                  showSpinner={false}
-                  value={input}
-                  onChange={(e) => setInput(e.target.value)}
-                  inputRef={inputref}
-                ></Textarea>
-              </div>
-              <div>
-                <label htmlFor="encoded" className={css(tw`text-white font-bold tracking-normal`)}>
-                  Output:{" "}
-                </label>
-                <div className={css(tw`relative`)}>
-                  <Spinner show={isSearching} className={css(tw`absolute top-2 text-secundaryFooter right-0 sm:mr-5 mr-5`)} />
-                  <div className={css(tw`font-mono h-32 pr-10 font-bold sm:text-base text-sm md:h-40 overflow-y-scroll break-words py-2 pl-3 mb-3 border-gray-100 mt-2 border `)}>
-                    <Highlighter
-                      className={css(!input ? tw`text-gray-400` : "")}
-                      searchWords={decodedData?.encodedWords ?? []}
-                      highlightStyle={{ background: "linear-gradient(45deg, #f7ff0088 0%, #db36a488 100%)", color: "white" }}
-                      autoEscape={true}
-                      textToHighlight={input ? input : "Decoded output"}
-                    />
-                  </div>
+          <div>
+            <div className={css(tw`w-full pt-6 `)}>
+              <div className={css(tw`md:grid grid-cols-2 col-gap-9`)}>
+                <div>
+                  <Textarea id={"inputD"} labelText={"Input text"} placeHolder={"Type or paste text"} showSpinner={false} value={input} onChange={(e) => setInput(e.target.value)} inputRef={inputRef}></Textarea>
                 </div>
-                <div className={css(tw`bg-white text-black rounded-sm `)}>
-                  <Stats data={decodedData!}></Stats>
+                <div>
+                  <label htmlFor="encoded" className={css(tw`text-white font-bold tracking-normal`)}>
+                    Output:{" "}
+                  </label>
+                  <div className={css(tw`relative`)}>
+                    <Spinner show={isSearching} className={css(tw`absolute top-2 text-secundaryFooter right-0 sm:mr-5 mr-5`)} />
+                    <div className={css(tw`font-mono h-32 pr-10 font-bold sm:text-base text-sm md:h-40 overflow-y-scroll break-words py-2 pl-3 mb-3 border-gray-100 mt-2 border `)}>
+                      <Highlighter
+                        className={css(!input ? tw`text-gray-400` : "")}
+                        searchWords={decodedData?.encodedWords ?? []}
+                        highlightStyle={{ background: "linear-gradient(45deg, #f7ff0088 0%, #db36a488 100%)", color: "white" }}
+                        autoEscape={true}
+                        textToHighlight={input ? input : "Decoded output"}
+                      />
+                    </div>
+                  </div>
+                  <div className={css(tw`bg-white text-black rounded-sm `)}>
+                    <Stats data={decodedData!}></Stats>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <Faq />
         </div>
-        <Faq />
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,18 @@
 import { css } from "@emotion/css";
 import tw from "@tailwindcssinjs/macro";
-import { Header } from "../components/Header";
-import { SwithPage } from "../components/SwitchPage";
-import { Button } from "../components/Button";
+import { Header } from "@/components/Header";
+import { SwithPage } from "@/components/SwitchPage";
+import { Button } from "@/components/Button";
 import { MdContentCopy, MdFileDownload } from "react-icons/md";
-import { Tooltip } from "../components/Tooltip";
+import { Tooltip } from "@/components/Tooltip";
 import CopyToClipboard from "react-copy-to-clipboard";
-import { Faq } from "../components/Faq";
-import { useState, useEffect, useRef } from "react";
-import { useDebouncedCallback, useDebounce } from "use-debounce";
-import { beautifyError } from "../utils/errorBeautify";
-import { Footer } from "../components/Footer";
-import { Textarea } from "../components/Textarea";
+import { Faq } from "@/components/Faq";
+import { useState, useRef } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import { useEncode } from "@/hooks/useEncode";
+import { Footer } from "@/components/Footer";
+import { Textarea } from "@/components/Textarea";
+import { Layout } from "@/components/Layout";
 
 const downloadTxtFile = (str: string, name: string) => {
   const element = document.createElement("a");
@@ -23,99 +24,64 @@ const downloadTxtFile = (str: string, name: string) => {
 };
 
 const Index = () => {
-  const [encodedData, setEncodedData] = useState<string | null>("");
+  const inputRef = useRef<HTMLTextAreaElement>(null);
   const [input, setInput] = useState<string>("");
-  const [isSearching, setIsSearching] = useState(false);
-  const [error, setError] = useState<boolean | null>(null);
-  const inputref = useRef<HTMLTextAreaElement>(null);
-  const [debouncedInputValue] = useDebounce(inputref.current?.value, 500);
-  const [debouncedLoading] = useDebounce(inputref.current?.value, 500, { leading: true });
-
-  useEffect(() => {
-    setIsSearching(true);
-    if (inputref.current?.value === "") {
-      setIsSearching(false);
-    }
-  }, [debouncedLoading]);
-
-  useEffect(() => {
-    setIsSearching(true);
-    if (debouncedInputValue === "") {
-      setEncodedData("");
-      setIsSearching(false);
-    }
-    debouncedInputValue &&
-      (async () => {
-        try {
-          const req = await fetch(`${window.location.origin}/api/encode`, {
-            body: JSON.stringify({ data: debouncedInputValue }),
-            method: "POST",
-          });
-          const dat = await req.json();
-          if (dat.error) throw new Error(dat.error);
-          setError(false);
-          setEncodedData(dat.encodedStr);
-        } catch (error) {
-          setEncodedData(beautifyError(error.message));
-          setError(true);
-        } finally {
-          setIsSearching(false);
-        }
-      })();
-  }, [debouncedInputValue]);
+  const { isSearching, error, encodedData } = useEncode({ inputRef });
 
   // for preventing spam clicking download button
-  const [debouncedDownaloadButtonCallback] = useDebouncedCallback((value) => {
+  const [debouncedDownaloadButtonCallback] = useDebouncedCallback(() => {
     downloadTxtFile(encodedData as string, "encoded.txt");
   }, 1000);
 
   return (
-    <div className={css(tw`bg-black`)}>
-      <div className={css(tw` max-w-screen-xl text-white pt-8 md:pt-10 md:px-6 px-8 flex flex-col ml-auto mr-auto`)}>
-        <div className={css(tw``)}>
+    <Layout>
+      <div className={css(tw`bg-black`)}>
+        <div className={css(tw` max-w-screen-xl text-white pt-8 md:pt-10 md:px-6 px-8 flex flex-col ml-auto mr-auto`)}>
           <div className={css(tw``)}>
-            <Header />
-          </div>
-        </div>
-
-        <div className={css(tw`text-white font-light pt-4 text-right`)}>
-          <SwithPage page={"decode"} />
-        </div>
-
-        <div>
-          <div className={css(tw`w-full pt-6 `)}>
-            <div className={css(tw`md:grid grid-cols-2 col-gap-9`)}>
-              <div>
-                <Textarea id={"inputD"} labelText={"Input text"} placeHolder={"Type or paste text"} showSpinner={false} value={input} onChange={(e) => setInput(e.target.value)} inputRef={inputref}></Textarea>
-              </div>
-              <div>
-                <Textarea id={"encoded"} hasError={error ?? false} labelText={"Output"} isReadOnly={true} hasSpinner={true} showSpinner={isSearching} value={encodedData as string} placeHolder={"Here you should see encoded text"}></Textarea>
-              </div>
-            </div>
-            <div
-              className={css(tw`inline-flex flex-col  text-white sm:flex-row`, {
-                float: "right",
-              })}
-            >
-              <div className={css(tw`sm:mr-2 mr-0 mb-2`)}>
-                <Tooltip disabled={error || encodedData === ""} className={css(tw`bg-white w-24 text-sm text-black font-medium`)} text={"✔ Copied!"} durationAfterLastClick={1500}>
-                  <CopyToClipboard text={encodedData as string}>
-                    <Button disabled={error || encodedData === ""} text={"Copy to clipboard"} className={css(tw`bg-primary-100 hover:bg-opacity-75 `)} icon={<MdContentCopy size="1rem" />} />
-                  </CopyToClipboard>
-                </Tooltip>
-              </div>
-              <div>
-                <Tooltip disabled={error || encodedData === ""} className={css(tw`bg-white w-40 text-black text-sm font-medium`)} text={"📁 Started download!"} durationAfterLastClick={1300}>
-                  <Button disabled={error || encodedData === ""} text={"Downaload as .txt"} className={css(tw`bg-secundary hover:bg-opacity-75`)} onClick={debouncedDownaloadButtonCallback as any} icon={<MdFileDownload size="1rem" />} />
-                </Tooltip>
-              </div>
+            <div className={css(tw``)}>
+              <Header />
             </div>
           </div>
+
+          <div className={css(tw`text-white font-light pt-4 text-right`)}>
+            <SwithPage page={"decode"} />
+          </div>
+
+          <div>
+            <div className={css(tw`w-full pt-6 `)}>
+              <div className={css(tw`md:grid grid-cols-2 col-gap-9`)}>
+                <div>
+                  <Textarea id={"inputD"} labelText={"Input text"} placeHolder={"Type or paste text"} showSpinner={false} value={input} onChange={(e) => setInput(e.target.value)} inputRef={inputRef}></Textarea>
+                </div>
+                <div>
+                  <Textarea id={"encoded"} hasError={error ?? false} labelText={"Output"} isReadOnly={true} hasSpinner={true} showSpinner={isSearching} value={encodedData as string} placeHolder={"Here you should see encoded text"}></Textarea>
+                </div>
+              </div>
+              <div
+                className={css(tw`inline-flex flex-col  text-white sm:flex-row`, {
+                  float: "right",
+                })}
+              >
+                <div className={css(tw`sm:mr-2 mr-0 mb-2`)}>
+                  <Tooltip disabled={error || encodedData === ""} className={css(tw`bg-white w-24 text-sm text-black font-medium`)} text={"✔ Copied!"} durationAfterLastClick={1500}>
+                    <CopyToClipboard text={encodedData as string}>
+                      <Button disabled={error || encodedData === ""} text={"Copy to clipboard"} className={css(tw`bg-primary-100 hover:bg-opacity-75 `)} icon={<MdContentCopy size="1rem" />} />
+                    </CopyToClipboard>
+                  </Tooltip>
+                </div>
+                <div>
+                  <Tooltip disabled={error || encodedData === ""} className={css(tw`bg-white w-40 text-black text-sm font-medium`)} text={"📁 Started download!"} durationAfterLastClick={1300}>
+                    <Button disabled={error || encodedData === ""} text={"Downaload as .txt"} className={css(tw`bg-secundary hover:bg-opacity-75`)} onClick={debouncedDownaloadButtonCallback as any} icon={<MdFileDownload size="1rem" />} />
+                  </Tooltip>
+                </div>
+              </div>
+            </div>
+          </div>
+          <Faq />
         </div>
-        <Faq />
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Layout>
   );
 };
 
